be59aa36659eb3e4df6d1603bdddc92081fc61a8
Xer0-PT <joelflm@gmail.com>
2023-01-19 00:56:34 +0000

    Add tests.

---
+++ tests/BlackList.Api.IntegrationTests/Fixtures/CustomWebApplicationFactory.cs
@@ -0,0 +1,75 @@
+﻿namespace BlackList.Api.IntegrationTests.Fixtures;
+
+using BlackList.Persistence.Data;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    private readonly TestcontainerDatabase _container;
+
+    public CustomWebApplicationFactory()
+    {
+        _container = new TestcontainersBuilder<PostgreSqlTestcontainer>()
+            .WithDatabase(new PostgreSqlTestcontainerConfiguration
+            {
+                Database = "postgres",
+                Username = "postgres",
+                Password = "postgres",
+            })
+            .WithImage("postgres:15.1")
+            .WithCleanUp(true)
+            .Build();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            // Remove AppDbContext
+            services.RemoveDbContext<BlackListServiceDbContext>();
+
+            // Add DB context pointing to test container
+            services.AddDbContext<BlackListServiceDbContext>(options => 
+            {
+                options.UseNpgsql(_container.ConnectionString); 
+            });
+
+            // Ensure schema gets created
+            services.EnsureDbCreated<BlackListServiceDbContext>();
+        });
+    }
+
+    public async Task InitializeAsync() => await _container.StartAsync();
+
+    public new async Task DisposeAsync() => await _container.DisposeAsync();
+}
+
+public static class ServiceCollectionExtensions
+{
+    public static void RemoveDbContext<T>(this IServiceCollection services) where T : DbContext
+    {
+        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<T>));
+        if (descriptor != null)
+        {
+            services.Remove(descriptor);
+        }
+    }
+
+    public static void EnsureDbCreated<T>(this IServiceCollection services) where T : DbContext
+    {
+        var serviceProvider = services.BuildServiceProvider();
+
+        using var scope = serviceProvider.CreateScope();
+        var scopedServices = scope.ServiceProvider;
+        var context = scopedServices.GetRequiredService<T>();
+        context.Database.EnsureCreated();
+    }
+}
---
+++ tests/BlackList.Api.UnitTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,34 @@
+﻿namespace BlackList.Api.UnitTests.Controllers;
+
+using BlackList.Api.Controllers;
+using BlackList.Application.Abstractions;
+using BlackList.Application.Dtos;
+using Moq;
+
+public class UserControllerTests
+{
+    private readonly Mock<IUserService> _service;
+    private readonly UserController _controller;
+
+    public UserControllerTests()
+    {
+        _service = new Mock<IUserService>();
+        _controller = new UserController(_service.Object);
+    }
+
+    [Fact]
+    public async void UserController_CreateUser_Creates()
+    {
+        // Arrange
+        _service
+            .Setup(x => x.CreateUserAsync(default))
+            .ReturnsAsync(It.IsAny<UserDto>())
+            .Verifiable();
+
+        // Act
+        var result = await _controller.CreateUser(default);
+
+        // Assert
+        _service.Verify();
+    }
+}
