34dc55bccdfe7830afaa8d369ae3d707278f0102
Joel Martins <38245877+Xer0-PT@users.noreply.github.com>
2023-01-25 23:45:36 +0000

    Refactor methods when a player is banned. (#28)

---
+++ src/BlackList.Application/Extensions/BlackListedPlayerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BlackList.Domain.Entities;
+
+namespace BlackList.Application.Extensions;
+
+public static class BlackListedPlayerExtensions
+{
+    public static bool PlayerIsAlreadyBanned(BlackListedPlayer? player)
+        => player is not null && player.Banned;
+
+    public static bool IsReban(BlackListedPlayer? player)
+        => player is not null && !player.Banned;
+
+    public static bool UserIsBanningHimself(string playerNickname, string userNickname)
+        => userNickname.Equals(playerNickname, StringComparison.OrdinalIgnoreCase);
+}
